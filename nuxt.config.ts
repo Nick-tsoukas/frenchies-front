c126763e12dbc34ea6cf664cee5708ba207c812d
Nikolaos Tsoukas <Nick-tsoukas>
2023-08-20 16:59:05 -0500

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  vite: {
+    server: {
+          fs: {
+            allow: ['../../../../../../../../../../../'],
+          },
+        },
+      },
+  devtools: { enabled: true }
+})
