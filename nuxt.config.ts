324724d04b8090eba5a3becc8f411b4f9bd640b3
Nikolaos Tsoukas <Nick-tsoukas>
2023-08-22 19:40:48 -0500

    chore: ...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-
+  ssr:false,
+  nitro: {
+    preset: 'service-worker'
+  },
   modules: [
     '@nuxt/content'
   ],
