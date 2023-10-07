18ed9be31c86226ca3b0302869db8e3794040871
Nikolaos Tsoukas <Nick-tsoukas>
2023-10-07 18:07:52 -0500

    chore: ...

--- pages/contact.vue
+++ pages/contact.vue
@@ -13,148 +13,28 @@
   ```
 -->
 <template>
-  <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-    <form name="contact" method="POST" data-netlify="true">
+  <div class="container mx-auto">
+    <div class="px-12 mt-12">
+      <h2 class="mb-6 text-4xl bold">Get in touch</h2>
       <p>
-        <label>Your Name: <input type="text" name="name" /></label>
+        If you're interested in welcoming a rare French Bulldog puppy into your
+        home, we're here to help! Please reach out via email on phone call for
+        any questions
       </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label
-          >Your Role:
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label
-        >
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-    <!-- <div class="mx-auto max-w-2xl text-center">
-      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-        Contact Us
-      </h2>
-      <p class="mt-2 text-lg leading-8 text-gray-600">
-        Have any questions about our puppies? Reach out and send us a message.
-        We are always happy to help find a pup for you.
-      </p>
+      <div class="my-8 flex flex-col gap-8">
+        <div class="flex items-center gap-4">
+          <NuxtImg height="20" width="20" src="/phone.png" alt="" />
+          <p>622-222-2222</p>
+        </div>
+        <div class="flex items-center gap-4">
+          <NuxtImg height="20" width="20" src="/map.png" alt="" />
+          <p>Aurora, IL</p>
+        </div>
+        <div class="flex items-center gap-4">
+          <NuxtImg height="20" width="20" src="/email.png" alt="" />
+          <p>puppies@houseoffrenchies.com</p>
+        </div>
+      </div>
     </div>
-    <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
-      <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div>
-          <label
-            for="first-name"
-            class="block text-sm font-semibold leading-6 text-gray-900"
-            >First name</label
-          >
-          <div class="mt-2.5">
-            <input
-              type="text"
-              name="first-name"
-              id="first-name"
-              autocomplete="given-name"
-              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <div>
-          <label
-            for="last-name"
-            class="block text-sm font-semibold leading-6 text-gray-900"
-            >Last name</label
-          >
-          <div class="mt-2.5">
-            <input
-              type="text"
-              name="last-name"
-              id="last-name"
-              autocomplete="family-name"
-              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-
-        <div class="sm:col-span-2">
-          <label
-            for="email"
-            class="block text-sm font-semibold leading-6 text-gray-900"
-            >Email</label
-          >
-          <div class="mt-2.5">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              autocomplete="email"
-              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <div class="sm:col-span-2">
-          <label
-            for="phone-number"
-            class="block text-sm font-semibold leading-6 text-gray-900"
-            >Phone number</label
-          >
-          <div class="relative mt-2.5">
-            <div class="absolute inset-y-0 left-0 flex items-center">
-              <label for="country" class="sr-only">Country</label>
-              <select
-                id="country"
-                name="country"
-                class="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-              >
-                <option>US</option>
-                <option>CA</option>
-                <option>EU</option>
-              </select>
-              <ChevronDownIcon
-                class="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </div>
-            <input
-              type="tel"
-              name="phone-number"
-              id="phone-number"
-              autocomplete="tel"
-              class="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <div class="sm:col-span-2">
-          <label
-            for="message"
-            class="block text-sm font-semibold leading-6 text-gray-900"
-            >Message</label
-          >
-          <div class="mt-2.5">
-            <textarea
-              name="message"
-              id="message"
-              rows="4"
-              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-      </div>
-      <div class="mt-10">
-        <button
-          type="submit"
-          class="block w-full rounded-md bg-[#303036] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Send Message
-        </button>
-      </div>
-    </form> -->
   </div>
 </template>
-
-<script setup></script>
